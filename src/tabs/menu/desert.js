d57d12b6ee8e69e9e626371dbcf914d4a8d71196
JR Nova <novakoda9@gmail.com>
2020-08-22 16:18:45 -0500

    added desert and wine menus

--- src/tabs/menu/desert.js
+++ src/tabs/menu/desert.js
@@ -1,4 +1,38 @@
 export const menuDesert = () => {
   let cont = document.getElementById('menu-container');
-  cont.innerHTML = "DESERT";
+  cont.innerHTML = `
+  <div class="menu-grid desert-grid">
+    <div class="menu-item">
+      <div id="churros" class="menu-item-pic">
+        <h3>Churros</h3>
+      </div>
+      <p class="menu-item-info">Sweet cinna-sugary goodness with a side of chocolate</p>
+      <h4 class="menu-item-price">$2.99</h4>
+    </div>
+
+    <div class="menu-item">
+      <div id="pasteis" class="menu-item-pic">
+        <h3>Pasteis</h3>
+      </div>
+      <p class="menu-item-info">Baked goodies with a fruity paste in the center</p>
+      <h4 class="menu-item-price">$1.99</h4>
+    </div>
+
+    <div class="menu-item">
+      <div id="smores" class="menu-item-pic">
+        <h3>Smores</h3>
+      </div>
+      <p class="menu-item-info">Made fresh over the campfire in the kitchen</p>
+      <h4 class="menu-item-price">$2.99</h4>
+    </div>
+
+    <div class="menu-item">
+      <div id="tiramisu" class="menu-item-pic">
+        <h3>Tiramisu</h3>
+      </div>
+      <p class="menu-item-info">Pretty much cake... I think</p>
+      <h4 class="menu-item-price">$2.99</h4>
+    </div>
+  </div>
+  `;
 }
--- src/tabs/menu/wine.js
+++ src/tabs/menu/wine.js
@@ -1,4 +1,61 @@
 export const menuWine = () => {
   let cont = document.getElementById('menu-container');
-  cont.innerHTML = "WINES";
+  cont.innerHTML = `
+    Note: We only sell bottles, not glasses.
+    <div class="menu-grid">
+
+      <div class="menu-item">
+        <div id="airen" class="menu-item-pic">
+          <h3>Airen</h3>
+        </div>
+        <h4 class="menu-item-price">$139.99</h4>
+      </div>
+
+      <div class="menu-item">
+        <div id="blanc" class="menu-item-pic">
+          <h3>Blanc</h3>
+        </div>
+        <h4 class="menu-item-price">$119.99</h4>
+      </div>
+
+      <div class="menu-item">
+        <div id="cabernet" class="menu-item-pic">
+          <h3>Cabernet</h3>
+        </div>
+        <h4 class="menu-item-price">$189.99</h4>
+      </div>
+
+      <div class="menu-item">
+        <div id="chardonnay" class="menu-item-pic">
+          <h3>Chardonnay</h3>
+        </div>
+        <h4 class="menu-item-price">$209.99</h4>
+      </div>
+
+      <div class="menu-item">
+        <div id="garnacha" class="menu-item-pic">
+          <h3>Garnacha</h3>
+        </div>
+        <h4 class="menu-item-price">$169.99</h4>
+      </div>
+
+      <div class="menu-item">
+        <div id="merlot" class="menu-item-pic">
+          <h3>Merlot</h3>
+        </div>
+        <h4 class="menu-item-price">$139.99</h4>
+      </div>
+
+      <div></div>
+
+      <div class="menu-item">
+        <div id="tempranillo" class="menu-item-pic">
+          <h3>Tempranillo</h3>
+        </div>
+        <h4 class="menu-item-price">$189.99</h4>
+      </div>
+
+    </div>
+
+  `;
 }
